b05f88e58463fc8fd12a501869e1deeb604c5c90
Youngjin Park <youngjin988@gmail.com>
2023-10-17 22:53:20 +0900

    refactor: 사용하지 않는 컴포넌트 삭제

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import Container from './components/Container/Container';
-import Grid from './components/Grid/Grid';
-import Flex from './components/Flex/Flex';
 import Drawer from './components/Drawer/Drawer';
 
-export { Container, Grid, Flex, Drawer };
+export { Drawer };
